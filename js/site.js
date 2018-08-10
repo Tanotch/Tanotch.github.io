22a29f7d454e66656a24d48c82f2d1fc25ca12d5
Breanna Tan <Breanna.Tan@ampf.com>
2018-08-10 17:33:43 -0500

    init commit

---
+++ js/site.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    //Navigation Click
+    $('.navbar .nav li a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+        $('.navbar-collapse').collapse('hide');
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 60
+        }, 300);
+    });
+
+    //Navigation Scroll
+    $("body").scrollspy({
+        target: ".navbar",
+        offset: 60
+    })
+
+});
